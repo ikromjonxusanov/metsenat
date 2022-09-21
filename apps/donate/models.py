f0b8c80f516affb3fabc0c49dbd0a20280e44e52
ikromjonxusanov <ikromjonkhusanov06@gmail.com>
2022-09-21 12:41:21 +0500

    donate model added status field

--- apps/donate/models.py
+++ apps/donate/models.py
@@ -9,10 +9,17 @@ class Donate(TimeStampedModel):
         Y = (0, "Yuridik shaxs")
         J = (1, "Jismoniy shaxs")
 
+    class Status(models.IntegerChoices):
+        New = (0, "Yangi")
+        InModeration = (1, "Moderatsiyada")
+        Confirmed = (2, "Tasdiqlangan")
+        Canceled = (3, "Bekor qilingan")
+
     fio = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=9, validators=[PhoneValidator])
     organization = models.CharField(max_length=60, null=True)
-    user_type = models.IntegerField(choices=UserType.choices)
+    user_type = models.PositiveSmallIntegerField(choices=UserType.choices)
+    status = models.PositiveSmallIntegerField(choices=Status.choices)
     amount = models.PositiveBigIntegerField()
 
     def __str__(self):
