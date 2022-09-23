a9b0289e452eb637b4a4051e922d2fb37d06793d
ikromjonxusanov <ikromjonkhusanov06@gmail.com>
2022-09-23 16:04:51 +0500

    ...

--- apps/statistic/views.py
+++ apps/statistic/views.py
@@ -1,7 +1,8 @@
-from django.db.models import Sum
+from django.db.models import Sum, Prefetch
 from rest_framework import views, permissions
 from rest_framework.response import Response
 
+from apps.donate.models import DonatesForStudent
 from apps.student.models import Student
 
 
@@ -10,7 +11,9 @@ class AdminMainStatisticsView(views.APIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get(self, request):
-        payed_amount = Student.objects.aggregate(amount=Sum('donater_for_student__amount'))['amount']
+        payed_amount = Student.objects.prefetch_related(
+            Prefetch('donater_for_student', queryset=DonatesForStudent.objects.only('amount'))
+        ).aggregate(amount=Sum('donater_for_student__amount'))['amount']
         asked_amount = Student.objects.aggregate(amount=Sum('contract_amount'))['amount']
         needed_amount = asked_amount - payed_amount
         return Response({
