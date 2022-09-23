54e3bba2d337beefc2e54cf57fa0cab2440d5498
ikromjonxusanov <ikromjonkhusanov06@gmail.com>
2022-09-23 16:30:24 +0500

    ...

--- apps/statistic/urls.py
+++ apps/statistic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from apps.statistic.views import AdminMainStatisticsView
+from apps.statistic.views import AdminMainStatisticsView, AdminDateStatistics
 
 urlpatterns = [
     path('admin/statistics/', include([
-        path('', AdminMainStatisticsView.as_view())
+        path('', AdminMainStatisticsView.as_view()),
+        path('date/', AdminDateStatistics.as_view())
     ]))
 ]
--- apps/statistic/views.py
+++ apps/statistic/views.py
@@ -1,8 +1,9 @@
-from django.db.models import Sum, Prefetch
+from django.db.models import Sum, Prefetch, Count
+from django.db.models.functions import TruncMonth, TruncDay
 from rest_framework import views, permissions
 from rest_framework.response import Response
 
-from apps.donate.models import DonatesForStudent
+from apps.donate.models import DonatesForStudent, Donate
 from apps.student.models import Student
 
 
@@ -20,4 +21,33 @@ class AdminMainStatisticsView(views.APIView):
             "payed_amount": payed_amount,
             "asked_amount": asked_amount,
             "needed_amount": needed_amount
+        })
+
+
+class AdminDateStatistics(views.APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request):
+        students_months = Student.objects.annotate(month=TruncMonth('created_at')) \
+            .values('month') \
+            .annotate(count=Count('id')) \
+            .values('month', 'count')
+        donates_months = Donate.objects.annotate(month=TruncMonth('created_at')) \
+            .values('month') \
+            .annotate(count=Count('id')) \
+            .values('month', 'count')
+        students_days = Student.objects.annotate(day=TruncDay('created_at')) \
+            .values('day') \
+            .annotate(count=Count('id')) \
+            .values('day', 'count')
+        donates_days = Donate.objects.annotate(day=TruncDay('created_at')) \
+            .values('day') \
+            .annotate(count=Count('id')) \
+            .values('day', 'count')
+
+        return Response({
+            "students_months": students_months,
+            "donates_months": donates_months,
+            "students_days": students_days,
+            "donates_days": donates_days,
         })
